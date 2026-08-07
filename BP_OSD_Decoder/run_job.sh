5bdd58f490ad745bac3f3d4cdd74d3e3f6ac6f41
HARSH070105 <harsh.kapoor@research.iiit.ac.in>
2026-08-07 13:06:40 +0530

    run 1

--- BP_OSD_Decoder/run_job.sh
+++ BP_OSD_Decoder/run_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
 #SBATCH --time=50:00:00
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=32
 #SBATCH --gres=gpu:0
 #SBATCH --mail-user=harsh.kapoor@research.iiit.ac.in
 #SBATCH --mail-type=ALL
@@ -16,9 +16,10 @@ hostname
 source /home2/harsh.kapoor/general/bin/activate
 
 # Run the script using the environment's python
-python simulate.py --code_name ghp_882_24 --error_rates 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.11 0.12 0.13 --trials 500000
-python simulate.py --code_name gb_254_28 --error_rates 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.11 0.12 0.13 --trials 500000
-python simulate.py --code_name gb_126_28 --error_rates 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.11 0.12 0.13 --trials 500000
+python simulate_GB_126.py
+python simulate_GB_254.py
+python simulate_GHP.py
+
 
 echo "END"
 date
