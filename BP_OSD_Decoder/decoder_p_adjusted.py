2322a11d36aa3fbe954cc50966bd108a4c51ce3a
HARSH070105 <harsh.kapoor@research.iiit.ac.in>
2026-08-17 13:12:06 +0530

    to ada part 1

--- BP_OSD_Decoder/decoder_p_adjusted.py
+++ BP_OSD_Decoder/decoder_p_adjusted.py
@@ -2,8 +2,8 @@ from ldpc.bposd_decoder import BpOsdDecoder
 from ldpc.bp_decoder import BpDecoder
 
 def setup_all_decoders(HX, HZ, p, osd_order=0):
-    px = 0.0
-    pz = p
+    px = 2*p/3
+    pz = 2*p/3
 
     bp_x = BpDecoder(
         HZ,
