a79603c647982cdb3be2c51441bfa14168b097fa
HARSH070105 <harsh.kapoor@research.iiit.ac.in>
2026-08-17 13:13:00 +0530

    to ada part 2

--- BP_OSD_Decoder/simulate_p_adjusted.py
+++ BP_OSD_Decoder/simulate_p_adjusted.py
@@ -65,7 +65,7 @@ def run_monte_carlo(filepath, error_rates, max_trials):
 
 if __name__ == "__main__":
     error_rates = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13]
-    trials = 50000
+    trials = 5000000
     
     code_name = "bt_tile_288_8_12"  # Change this to the desired code name (without .npz extension)
     print(f"--- Simulating Code: {code_name} ---")
